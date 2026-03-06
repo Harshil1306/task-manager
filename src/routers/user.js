d9e9bc91eec924586e03c20f0beb2e5f77a2d94c
Y S Harshil <ysharshil@gmail.com>
2026-03-06 23:36:40 +0530

    add password validation middelware

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,6 +8,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
+        console.log(user)
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -71,7 +72,10 @@ router.patch('/users/:id', async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+        const user = await User.findById(_id);
+        updates.forEach((update) => user[update] = req.body[update]);
+        await user.save();
+        // const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
         if (!user) {
             return res.status(404).send();
         }
