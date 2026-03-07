4bff5fcc20b186234f2e0c62f988439321011502
Y S Harshil <ysharshil@gmail.com>
2026-03-07 09:24:32 +0530

    add login route

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -40,6 +41,23 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+
+}
 
 userSchema.pre('save', async function (next) {
     const user = this
